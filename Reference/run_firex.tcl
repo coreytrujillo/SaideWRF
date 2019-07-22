a3417b8197f3e7050029e718bd505007f4d2173f
Corey Trujillo <corey.trujillo@colorado.edu>
2019-07-22 15:38:21 -0600

    First try at wps

--- Reference/run_firex.tcl
+++ Reference/run_firex.tcl
@@ -1,8 +1,9 @@
-# This script runs the tracer version of WRF for FIREX-AQ
-# It is assumed that WRF and WPS are already compiled
-
 #!/usr/bin/wish -f
 
+# This script runs the tracer version of WRF for FIREX-AQ
+# It is assumed that WRF and WPS are already compiled
+# Environment vars and modules should be sourced in the submit script
+
 # ###########################################################################
 #
 # 
@@ -13,7 +14,7 @@
 
 # ###########################################################################
 #
-# Set variables, modules, and options
+# Set variables and options
 # 
 # ###########################################################################
 
@@ -28,8 +29,54 @@ set WPS_DIR [file join $MAIN_DIR wps]
 set WRF_DIR [file join $MAIN_DIR WRF]
 set OUT_DIR [file join $MAIN_DIR Output]
 
-# Source Environment Variables
-eval "source $REF_DIR/Pleiades_EnvVars.sh"
+# On/off Options
+set wps "yes"
+
+
+
+# Initialize log
+set log_name [file join $REF_DIR run_[clock format [clock seconds] -format "%Y-%m-%d_%H:%M:%S" ]\.log]
+set log [open $log_name +w]
+
+# ###########################################################################
+#
+# Define Dates
+# 
+# ###########################################################################
+
+
+# Define start date and time
+set start_date_str "20190619"; # Start Date
+set shh 06; # Simulation start hour in Mountain time
+
+if { $start_date_str > 0} {
+	set start_date [string range $start_date_str 0 3] ;# Year
+	lappend start_date [string range $start_date_str 4 5] ;# Month
+	lappend start_date [string range $start_date_str 6 7] ;# Day
+	
+	} else {
+	set secondi [clock seconds]
+	# Convert MDT to UTC: +6 hours  
+	set secondi [expr $secondi + 3600*6]; 
+	set start_date [split [clock format $secondi -format %Y:%m:%d] ":"] ;
+}
+
+set syyyy [lindex $start_date 0] ;# Year
+set smm   [lindex $start_date 1] ;# Month
+set sdd   [lindex $start_date 2] ;# Day
+set sdate $syyyy$smm$sdd
+
+# Define simulation length in hours
+set time_step 84
+
+# Set final date variables based on simulation lengths
+append end_date [lindex $start_date 0] [lindex $start_date 1] [lindex $start_date 2]
+set end_date [expr [clock scan $end_date]+[expr $time_step*3600]]
+set eyyyy [clock format $end_date -format %Y]
+set emm   [clock format $end_date -format %m]
+set edd   [clock format $end_date -format %d]
+set ehh   [clock format $end_date -format %H]
+
 
 
 # ###########################################################################
@@ -44,8 +91,27 @@ eval "source $REF_DIR/Pleiades_EnvVars.sh"
 # Run WPS
 # 
 # ###########################################################################
+if { $wps == "yes" }{
+	puts $log "# ---------------------------------------------------------"
+	flush $log
+	
+	cd $WPS_DIR
+	
+	# Clean up leftover files: SST, FILE, GRIB, met_em, gfs, gdas, nam
+	if {[llength [glob -nocomplain [file join SST*]]] > 0} {eval "file delete -force [glob -nocomplain [file join SST*]]"}
+	if {[llength [glob -nocomplain [file join  FILE:*]]] > 0} {eval "file delete -force [glob -nocomplain [file join FILE:*]]"}
+	if {[llength [glob -nocomplain [file join  GRIBFILE*]]] > 0} {eval "file delete -force [glob -nocomplain [file join GRIBFILE*]]"}
+	if {[llength [glob -nocomplain [file join met_em.d*]]] > 0} {eval "file delete -force [glob -nocomplain [file join met_em.d*]]"}
+	if {[llength [glob -nocomplain [file join gfs*]]] > 0} {eval "file delete -force [glob -nocomplain [file join gfs*]]"}
+	if {[llength [glob -nocomplain [file join gdas*]]] > 0} {eval "file delete -force [glob -nocomplain [file join gdas*]]"}
+	if {[llength [glob -nocomplain [file join *nam*]]] > 0} {eval "file delete -force [glob -nocomplain [file join *nam*]]"}
 
-
+	# Link NAM data
+	set c [catch { eval "exec ./link_grib.csh [file join $DATA_DIR NAM $sdate *nam.*]" } msg]
+	
+	
+	
+}
 
 # ###########################################################################
 #
